b9a66f1addee00fded4a13c529d78c5fef70b1f7
shafeeqahd <31110727+shafeeqahd@users.noreply.github.com>
2023-09-08 16:38:10 -0400

    Added Breadth First Search

--- Chapter6/breadth_first.py
+++ Chapter6/breadth_first.py
@@ -6,7 +6,8 @@ from collections import deque
 graph = {
     "You": ["alice", "bob", "claire"],
     "bob": ["anuj", "peggy"],
-    "alice": ["thon", "jonny"],
+    "alice": ["peggy"],
+    "claire": ["thon", "jonny"],
     "anuj": [],
     "peggy": [],
     "thon": [],
@@ -15,7 +16,34 @@ graph = {
 
 
 def person_is_seller(name):
+    """
+    criteria to check if answer is found
+    :param name: Name of the connection
+    :return: Boolean
+    """
     return name[-1] == "n"
 
 
-search_queue = deque()
+def breadth_first_search(name):
+    """
+    Breadth First algorithm
+    :param name: Input Name
+    :return: Name of the connection if found, Else returns False
+    """
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+                return True
+            search_queue += graph[person]
+            searched.append(person)
+    return False
+
+
+if __name__ == "__main__":
+    name_ip = input("What is the name?")
+    breadth_first_search(name_ip)
